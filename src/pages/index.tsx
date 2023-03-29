59eb95cf08e383a4e479e3891928cbb1980202dc
beefreguglia <beefreguglia@gmail.com>
2023-03-29 12:15:52 -0300

    feature: instal lint and lint project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import { Heading } from "@ignite-ui/react";
+import { Heading } from '@ignite-ui/react'
 
 export default function Home() {
-  return (
-    <Heading>hello world</Heading>
-  )
+  return <Heading>hello world</Heading>
 }
