6e7a3c502b17ee89b3c9b49e0380195086cf070a
beefreguglia <beefreguglia@gmail.com>
2023-04-04 17:48:44 -0300

    feat: connect confirm form with react hook form

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,11 +1,38 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
 import { CalendarBlank, Clock } from 'phosphor-react'
-import { ConfirmForm, ConfirmFormActions, ConfirmFormHeader } from './styles'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import {
+  ConfirmForm,
+  ConfirmFormActions,
+  ConfirmFormError,
+  ConfirmFormHeader,
+} from './styles'
+
+const confirmFormSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa de pelo menos 3 caracteres' }),
+  email: z.string().email({ message: 'Digite um e-mail válido' }),
+  observations: z.string().nullable(),
+})
+
+type ConfirmFormData = z.infer<typeof confirmFormSchema>
 
 export function ConfirmStep() {
-  function handleConfirmScheduling() {}
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<ConfirmFormData>({
+    resolver: zodResolver(confirmFormSchema),
+  })
+  function handleConfirmScheduling(data: ConfirmFormData) {
+    console.log(data)
+  }
   return (
-    <ConfirmForm as="form">
+    <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
       <ConfirmFormHeader>
         <Text>
           <CalendarBlank />
@@ -18,21 +45,33 @@ export function ConfirmStep() {
       </ConfirmFormHeader>
       <label>
         <Text size="sm">Nome Completo</Text>
-        <TextInput placeholder="Seu nome" />
+        <TextInput placeholder="Seu nome" {...register('name')} />
+        {errors.name && (
+          <ConfirmFormError size="sm">{errors.name.message}</ConfirmFormError>
+        )}
       </label>
       <label>
         <Text size="sm">Endereço de e-mail</Text>
-        <TextInput type="email" placeholder="johndoe@example.com" />
+        <TextInput
+          type="email"
+          placeholder="johndoe@example.com"
+          {...register('email')}
+        />
+        {errors.email && (
+          <ConfirmFormError size="sm">{errors.email.message}</ConfirmFormError>
+        )}
       </label>
       <label>
         <Text size="sm">Observações</Text>
-        <TextArea />
+        <TextArea {...register('observations')} />
       </label>
       <ConfirmFormActions>
         <Button type="button" variant="tertiary">
           Cancelar
         </Button>
-        <Button type="submit">Confirmar</Button>
+        <Button type="submit" disabled={isSubmitting}>
+          Confirmar
+        </Button>
       </ConfirmFormActions>
     </ConfirmForm>
   )
