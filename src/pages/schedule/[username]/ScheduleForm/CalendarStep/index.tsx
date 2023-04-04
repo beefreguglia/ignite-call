d74e7ea314fcdc1879e6bb38f018ba6456c280ca
beefreguglia <beefreguglia@gmail.com>
2023-04-04 10:30:20 -0300

    feat: create components of calendar

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from '@/src/components/Calendar'
+import { CalendarStepContainer } from './styles'
+
+export default function CalendarStep() {
+  return (
+    <CalendarStepContainer>
+      <Calendar />
+    </CalendarStepContainer>
+  )
+}
